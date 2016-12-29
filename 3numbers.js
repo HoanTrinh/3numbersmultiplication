98b5a2aa553749066cc7340a4ba78994a01ad4f8
Shamoon Siddiqui <shamoon@develop.io>
2016-12-29 11:45:34 -0500

    Fixed

--- 3numbers.js
+++ 3numbers.js
@@ -1,15 +1,21 @@
-var num1 = parseInt(Math.random() * 100);
-var num2 = parseInt(Math.random() * 100);
-var num3 = parseInt(Math.random() * 100);
+var num1, num2, num3;
 
-var answer = num1 * num2*num3;
-var remainder = answer % 2;
+do {
+  num1 = parseInt(Math.random() * 100);
+  num2 = parseInt(Math.random() * 100);
+  num3 = parseInt(Math.random() * 100);
 
-console.log('Num1: ', num1);
-console.log('Num2: ', num2);
-console.log('Num3: ', num3);
-do{
-   var num1 * var num2 * var num3
-    if var answer = x% 2
+  console.log('Num1: ', num1);
+  console.log('Num2: ', num2);
+  console.log('Num3: ', num3);
+  console.log('');
+} while (isEven(num1, num2, num3));
+
+function isEven(num1, num2, num3) {
+  var answer = num1 * num2 * num3;
+
+  if(answer % 2 === 0) {
+    return true;
+  }
+  return false;
 }
-while
